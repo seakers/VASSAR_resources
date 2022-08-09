dbf2b8baa78493d21a63ead9ba42f5f86a9d1967
gapaza <gapaza@tamu.edu>
2022-08-09 15:25:28 -0500

    now enters blank datasets for problems is no dataset file

--- db_utility/models/Architectures.py
+++ db_utility/models/Architectures.py
@@ -14,16 +14,18 @@ class Architectures:
 
 
     def index(self):
+
+        # --> Index Datasets
         for problem in self.problems:
-            if problem != "SMAP" and problem != "SMAP_JPL1" and problem != "SMAP_JPL2":
-                continue
             problem_id = self.client.get_problem_id(problem)
             dataset_id = self.client.index_dataset("default", problem_id, None, None)
             filepath = '/app/daphne/VASSAR_resources/problem_default_archs/' + problem + '/default.csv'
-            df = pd.read_csv(filepath, header=0)
-            for index, row in df.iterrows():
-                input = row[0]
-                science = row[1]
-                cost = row[2]
-                self.client.index_architecture(problem_id, dataset_id, input, science, cost)
+            if os.path.isfile(filepath):
+                df = pd.read_csv(filepath, header=0)
+                for index, row in df.iterrows():
+                    input = row[0]
+                    science = row[1]
+                    cost = row[2]
+                    self.client.index_architecture(problem_id, dataset_id, input, science, cost)
+
 
